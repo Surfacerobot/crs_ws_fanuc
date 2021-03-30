6ab5c79674ed3cde88bf263c31d8ff7d1364d7d8
xiaopeng@surobopilot.com <xiaopeng@surobopilot.com>
2021-03-30 03:57:15 +0000

    2021-3-30 modify motion

--- src/collaborative-robotic-sanding/crs_gui/src/widgets/crs_application_widget.cpp
+++ src/collaborative-robotic-sanding/crs_gui/src/widgets/crs_application_widget.cpp
@@ -80,7 +80,7 @@ CRSApplicationWidget::CRSApplicationWidget(rclcpp::Node::SharedPtr node, QWidget
   namespace fs = boost::filesystem;
   ui_->setupUi(this);
 //  QImage *image= new QImage("../../ui/SURFACE_LOGO-01.png");:/new/prefix1/collaborative-robotic-sanding/crs_gui/ui/SURFACE_LOGO-01.png
-  ui_->label->setPixmap(QPixmap("/root/crs_ws_fanuc/src/collaborative-robotic-sanding/crs_gui/ui/SURFACE_LOGO-01.png"));
+//  ui_->label->setPixmap(QPixmap("/root/crs_ws_fanuc/src/collaborative-robotic-sanding/crs_gui/ui/SURFACE_LOGO-01.png"));
 
 
   // Set up ROS Interfaces to crs_application
